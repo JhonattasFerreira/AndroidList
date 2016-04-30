9883edd88e49181d01b6ce03364a89bd584d3d0d
Jhonattas Ferreira <jfs4@cin.ufpe.br>
2016-04-30 02:45:44 -0300

    Add delete

--- ToDoList/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> tarefas = new ArrayList();
+    int number = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +23,52 @@ public class MainActivity extends AppCompatActivity {
         String textFieldContent = textField.getText().toString();
         if (!textFieldContent.isEmpty()){
             tarefas.add(textFieldContent);
+            display();
         }
         textField.getText().clear();
-        display();
+
     }
 
     public void display(){
-        TextView things = (TextView) findViewById(R.id.things_field);
 
+        TextView things = (TextView) findViewById(R.id.things_field);
         String list = "";
+
         for (String x : tarefas ) {
-            list = list + "\n" + x + "\n";
+            list = list + x + "\n";
         }
 
         things.setText(list);
+    }
+
+    public void displayNumber(){
+        TextView numberTextView = (TextView) findViewById(R.id.number_text_view);
+        numberTextView.setText("" + number);
+    }
+
+    public void decrement(View view){
+        if (number > 1){
+            number = number - 1;
+            displayNumber();
+        }
+
+    }
+
+    public void increment(View view){
+        if (number < tarefas.size()){
+            number = number + 1;
+            displayNumber();
+        }
+    }
+
+    public void deletar(View view){
+        if(number != 0){
+            tarefas.remove((number - 1));
+            number = 0;
+            display();
+            displayNumber();
+        }
+
     }
 
 }
